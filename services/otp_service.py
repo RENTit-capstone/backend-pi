b270367d2bb714dfff88e935119e904b1d55458e
gurbur <jihwan7577@gmail.com>
2025-05-01 19:41:33 +0900

    Refactor: otp_service.py

--- services/otp_service.py
+++ services/otp_service.py
@@ -1,12 +1,12 @@
-# services/otp_service.py
+import time
 from services import mqtt_client
 from services.config import settings
-import time
 
 university_id = settings.UNIVERSITY_ID
 locker_id = settings.LOCKER_ID
 
-def send_otp_verification(otp: str, action: str):
+
+def send_otp_verification(otp: str, action: str) -> None:
     topic = f"event/{university_id}/{locker_id}/otp_verification"
     payload = {
         "otp": otp,
@@ -15,4 +15,4 @@ def send_otp_verification(otp: str, action: str):
     }
 
     mqtt_client.publish(topic, payload)
-    print(f"[OTP_SERVICE] Published verification request: {payload}")
+    print(f"[OTP_SERVICE] Sent OTP verification: {payload}")
