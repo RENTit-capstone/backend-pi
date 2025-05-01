8f9d7f6e9f222a897912193dd3614c9fae767c4c
gurbur <jihwan7577@gmail.com>
2025-05-01 19:39:29 +0900

    Refactor: config.py

--- services/config.py
+++ services/config.py
@@ -1,13 +1,14 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-  MQTT_BROKER: str
-  MQTT_PORT: int
-  MQTT_KEEPALIVE: int = 60
-  UNIVERSITY_ID: str
-  LOCKER_ID: str
+    MQTT_BROKER: str
+    MQTT_PORT: int
+    MQTT_KEEPALIVE: int = 60
+    UNIVERSITY_ID: str
+    LOCKER_ID: str
 
-  class Config:
-    env_file = "../.env"
-  
+    model_config = {
+        "env_file": "../.env"
+    }
+
 settings = Settings()
