5840ce6706df12846ab3ba38d806417a1c61003c
gurbur <jihwan7577@gmail.com>
2025-05-12 12:29:45 +0900

    Fix: qos to 1

--- services/mqtt_client.py
+++ services/mqtt_client.py
@@ -38,7 +38,7 @@ def publish(topic: str, message: dict) -> None:
     try:
         json_message = json.dumps(message)
         print(f"[MQTT] Publishing to {topic}: {json_message}")
-        mqtt_client.publish(topic, json_message)
+        mqtt_client.publish(topic, json_message, qos = 1)
     except Exception as e:
         print(f"[MQTT] Failed to publish to {topic}: {e}")
 
